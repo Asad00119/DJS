1120ba1c4777e7ef0348e8499ac57643bcf5e456
Dr-Johnny <153152625+Dr-Johnny@users.noreply.github.com>
2023-12-20 11:44:33 +0500

    Update djs.py

--- djs.py
+++ djs.py
@@ -5,6 +5,6 @@ print('\033[92;1m Join Whatsapp Group')
 os.system('xdg-open https://chat.whatsapp.com/JSafACOY70V65BmerDkeRC')
 djs=platform.architecture()[0]
 if djs=="32bit":
-    __import__("doc1")
+    __import__("doc")
 elif djs=="64bit":
     __import__("doc1")
